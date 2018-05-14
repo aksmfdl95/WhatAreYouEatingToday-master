b2f02ae08bd1668deefbd787d26371087d315112
sky <aksmfdl95@naver.com>
2018-05-14 17:02:07 +0900

    first commit

--- app/src/main/java/com/hnsky/whatareyoueatingtoday/TabFragment1.java
+++ app/src/main/java/com/hnsky/whatareyoueatingtoday/TabFragment1.java
@@ -3,13 +3,14 @@ package com.hnsky.whatareyoueatingtoday;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-public class TabFragment1 extends Fragment {
+public class TabFragment1 extends Fragment{
 
 
     @Nullable
